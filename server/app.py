deb08f2295b1ee445f3c0ed30644d430e97cccd6
Volodymyr Fihurniak <volodymyr.fihurniak@gmail.com>
2024-04-10 21:58:50 +0300

    Update server configuration to support HTTPS

--- server/app.py
+++ server/app.py
@@ -21,7 +21,7 @@ class App:
         self.config_env = dotenv_values('configs/.env')
         with open('configs/config.yaml', encoding='utf-8') as file:
             self.config_yaml = load(file, Loader=Loader)
-        self.config = Config(self.config_yaml, self.config_env)
+        self.config = Config(self.config_env, self.config_yaml)
         self.logging = Logger(self.config)
         self.router = APIRouter()
 
@@ -36,7 +36,21 @@ class App:
 
     def run(self):
         self.app.include_router(self.get_routes())
-        server_run(
-            self.app, host="0.0.0.0", port=3000,
-            log_config=None, log_level="info",
-        )
+        if (self.config.app.use_https):
+            ssl_keyfile = None
+            ssl_certfile = None
+            with open(self.config.app.ssl_key_path, 'rb') as file:
+                ssl_keyfile = file.read()
+            with open(self.config.app.ssl_cert_path, 'rb') as file:
+                ssl_certfile = file.read()
+            server_run(
+                self.app, host=self.config.app.host, port=self.config.app.port,
+                log_config=None, log_level="info",
+                ssl_keyfile=ssl_keyfile,
+                ssl_certfile=ssl_certfile,
+            )
+        else:
+            server_run(
+                self.app, host=self.config.app.host, port=self.config.app.port,
+                log_config=None, log_level="info"
+            )
--- server/entities/config.py
+++ server/entities/config.py
@@ -1,61 +1,57 @@
 from __future__ import annotations
 from collections import OrderedDict
-from pydantic import BaseModel, ValidationError
 
 
-class AppYAMLConfig(BaseModel):
+class AppModelConfig:
     host: str
     port: int
     use_https: bool
+    ssl_key_path: str
+    ssl_cert_path: str
+
+    def __init__(self, env: OrderedDict, config: dict) -> None:
+        self.host = env.get('app_host') or config['app']['host']
+        self.port = int(env.get('app_port')) or int(config['app']['port'])
+        self.use_https = bool(env.get('app_use_https')) or bool(
+            config['app']['use_https'])
+        self.ssl_key_path = env.get(
+            'app_ssl_key_path') or config['app']['ssl_key_path']
+        self.ssl_cert_path = env.get(
+            'app_ssl_cert_path') or config['app']['ssl_cert_path']
 
 
-class DBYAMLConfig(BaseModel):
+class DBModelConfig:
     host: str
     port: int
     user: str
     password: str
     database: str
 
+    def __init__(self, env: OrderedDict, config: dict) -> None:
+        self.host = env.get('db_host') or config['db']['host']
+        self.port = env.get('db_port') or config['db']['port']
+        self.user = env.get('db_user') or config['db']['user']
+        self.password = env.get('db_password') or config['db']['password']
+        self.database = env.get('db_database') or config['db']['database']
 
-class LoggingYAMLConfig(BaseModel):
+
+class LoggingModelConfig:
     config_path: str
     log_dir: str
 
-
-class AppDotEnvConfig(BaseModel):
-    app_host: str
-    app_port: int
-    app_use_https: bool
-
-
-class DBDotEnvConfig(BaseModel):
-    db_host: str
-    db_port: int
-    db_user: str
-    db_password: str
-    db_database: str
-
-
-class LoggingDotEnvConfig(BaseModel):
-    logging_config_path: str
-    logging_log_dir: str
+    def __init__(self, env: OrderedDict, config: dict) -> None:
+        self.config_path = env.get(
+            'logging_config_path') or config['logging']['config_path']
+        self.log_dir = env.get(
+            'logging_log_dir') or config['logging']['log_dir']
 
 
 class Config:
-    app: AppYAMLConfig | AppDotEnvConfig = None
-    db: DBYAMLConfig | DBDotEnvConfig = None
-    logging: LoggingYAMLConfig | LoggingDotEnvConfig = None
+    app: AppModelConfig = None
+    db: DBModelConfig = None
+    logging: LoggingModelConfig = None
 
-    def __init__(self, config: dict, env:  OrderedDict) -> None:
-        try:
-            self.app = AppDotEnvConfig(**env)
-        except ValidationError:
-            self.app = AppYAMLConfig(**config['app'])
-        try:
-            self.db = DBDotEnvConfig(**env)
-        except ValidationError:
-            self.db = DBYAMLConfig(**config['db'])
-        try:
-            self.logging = LoggingDotEnvConfig(**env)
-        except ValidationError:
-            self.logging = LoggingYAMLConfig(**config['logging'])
+    def __init__(self, env: OrderedDict, config: dict) -> None:
+        self.app = AppModelConfig(env, config)
+        self.db = DBModelConfig(env, config)
+        self.logging = LoggingModelConfig(env, config)
